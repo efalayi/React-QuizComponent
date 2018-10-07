d797efe298896b61102f8c2e478f7f8571cb429f
Esther Falayi <esther.falayi@andela.com>
2018-10-07 21:55:38 +0100

    feat(component): create QuizEnd component
    - create new QuizEnd compoent
    - conditionally render component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion'
+import QuizEnd from './QuizEnd'
 
 let quizData = require('./quiz_data.json');
 
@@ -7,16 +8,21 @@ class Quiz extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      quiz_position: 1
+      quiz_position: 2
     }
 
   }
   render() {
+    const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
     return (
       <div>
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-        />
+        {
+          isQuizEnd ?
+          <QuizEnd /> :
+          <QuizQuestion
+            quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+          />
+        }
       </div>
     )
   }
